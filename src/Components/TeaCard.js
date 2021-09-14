0ad8c32c318c742e0bb389f211994f30d20409cd
+ <+>
2021-09-14 16:29:08 -0400

    got it up and running and mapping and listing done

--- src/Components/TeaCard.js
+++ src/Components/TeaCard.js
@@ -2,12 +2,17 @@ import React from "react";
 
 function TeaCard ({tea}) {
     const {id, name, image, caffeinated, category, rating, description, price, keywords} = tea
-    console.log(tea)
+
     
     return (
         <li>
-            
-            <img key={id} src={image} alt={name} />
+            <img key={id} src={image} alt={name} height="100" width="100"/>
+            <h3>{name}</h3>
+            <p>{category}</p>
+            {caffeinated ? 
+            (<h4>"Caffein"</h4>) : 
+            (<h4>"Decaff"</h4>)}
+            <p>{rating}</p>
         </li>
 
     )
