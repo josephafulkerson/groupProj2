506e2fb86c7ebac51e4bf4568e3f5d1794914fdb
koltvictor <80918917+koltvictor@users.noreply.github.com>
2021-09-14 15:37:20 -0700

    Update TeaPage.js

--- src/Components/TeaPage.js
+++ src/Components/TeaPage.js
@@ -15,14 +15,10 @@ function TeaPage () {
             {/* <h1>House Tea Made Locally</h1> */}
             <br />
              {/* <Search /> */}
-<<<<<<< HEAD
              <br />
-             <TeaList itemsPerRow={6}
-             teas={teas}/>
-=======
-            <br />
+             <TeaList teas={teas}/>
+             <br />
             <TeaList teas={teas}/>
->>>>>>> 51dc4bfad7f15b7c9b0238c4388f17a55e8e3dc9
         </div>
     )
 }
